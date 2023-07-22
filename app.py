958760b453fc7ebb8dc2d732ec8d707e7055aaaa
ritish <sritish.10@gmail.com>
2023-07-22 18:30:34 +0530

    added app.py

--- app.py
+++ app.py
@@ -4,25 +4,39 @@ import time
 import pickle
 from utils import search
 from sentence_transformers import SentenceTransformer
-
+import streamlit   as st
 
 #PATHS
-STATUTE_MODEL= SentenceTransformer(r'models\msmarco-distilbert-base-v4-e1')
+STATUTE_MODEL= SentenceTransformer(r'models/msmarco-distilbert-base-v4-e1')
+CASE_MODEL= SentenceTransformer(r'models/msmarco-distilbert-base-v4-e4-case')
 
 
 app = FastAPI()
 
+
+@app.get("/")
+def start():
+    try:
+        return {'code':200}
+    
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return {"error":message}
+
+
 @app.get("/statute/")
 async def get_statute(query: str):
     try:
-        start_time = time.time()
+        start_time = time.time()            
         
-        vectorizer=pickle.load(open(r'models\vectorizer.pkl','rb'))
+        vectorizer=pickle.load(open(r'models/vectorizer.pkl','rb'))
         tfidf_vector=vectorizer.transform([query]).toarray().astype('float32')
 
         sbert_vector=STATUTE_MODEL.encode([query])
         
         articles=search.search_article(tfidf_vector,sbert_vector)
+        print(articles)
 
         return {'code':200,'statute':articles,'time':time.time()-start_time}
 
@@ -30,4 +44,26 @@ async def get_statute(query: str):
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         return {"error":message}
+    
 
+
+@app.get("/case/")
+async def get_case(query: str):
+    try:
+        start_time = time.time()            
+        
+        # vectorizer=pickle.load(open(r'models/case_judgement_tfidf_vectorizer.pkl','rb'))
+        # tfidf_vector=vectorizer.transform([query]).toarray().astype('float32')
+
+        sbert_vector=CASE_MODEL.encode([query])
+        
+        cases=search.search_case(sbert_vector)
+
+        print(cases)
+
+        return {"code":200,"case":str(cases),"time":time.time()-start_time}
+
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        return {"error":message}
